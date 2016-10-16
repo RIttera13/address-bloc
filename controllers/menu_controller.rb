3c717b7269c9393099a9c6139f28a71f05f6583b
Drew <rittera13@gmail.com>
2016-10-16 10:15:40 -0500

    Ruby assignement 6, created a 'view_entry_number' method that will retrieve the address in the array position requested by the user, with an offset of '1' for a more natural user experience. From the users perspective the array will start at 1. If a number is provided outside of the array, a message prompting the user for a new number will be displayed.

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - View Entry Number n"
+    puts "6 - Exit"
     print "Enter your selection: "
     selection = gets.to_i
 
@@ -35,6 +36,9 @@ class MenuController
         read_csv
         main_menu
       when 5
+        view_entry_number
+        main_menu
+      when 6
         puts "Good-bye!"
         exit(0)
       else
@@ -75,6 +79,23 @@ class MenuController
   end
 
   def read_csv
+  end
+
+  def view_entry_number
+
+    puts "Address Book Number?"
+
+    selection = gets.chomp.to_i
+
+    case
+    when selection <= address_book.entries.count
+        system"clear"
+        puts address_book.entries[selection-1].to_s
+      else
+        system "clear"
+        puts "#{selection} Record does not exist, please enter a new number."
+        view_entry_number
+    end
   end
 
   def entry_submenu(entry)
